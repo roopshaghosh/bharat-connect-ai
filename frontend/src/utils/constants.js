f85806eb75fa74ae6027ceaceeea2bb448e6df7c
Roopsha Ghosh <roopshaghosh2006@gmail.com>
2026-06-22 17:46:47 +0530

    chore: add deployment configurations for Render and Vercel

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:5000/api';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 export const BLOOD_GROUPS = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
 
